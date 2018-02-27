8a0a95b539ccbb492a9bf6fce527cd39a9a3fd32
Andrei Mandychev <amandychev@accengage.com>
2018-02-27 18:15:27 +0100

    Display Android image on the MainActivity

--- MyDummyApp/app/src/main/java/com/andronmobi/mydummyapp/MainActivity.kt
+++ MyDummyApp/app/src/main/java/com/andronmobi/mydummyapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.andronmobi.mydummyapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val imageUrl = "https://github.com/andronmobi/android-reverse-engineering/raw/master/images/android.png"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        GlideApp.with(this).load(imageUrl).into(imageView)
     }
 }
